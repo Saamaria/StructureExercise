b4d117e133ccf1bf799d29d0917e2d8e522790b8
Saamaria <67989662+Saamaria@users.noreply.github.com>
2020-07-27 23:15:19 -0500

    Add files via upload

---
+++ Saamaria_B_FunctionExercisee.playground/Contents.swift
@@ -0,0 +1,9 @@
+struct Resolution {
+    var width = 1080
+    var height = 720
+}
+    let myResolution = Resolution()
+   
+print("The width of my awesome new monitor is \(myResolution.width)")
+print("The height of my awesome new monitor is \(myResolution.height)")
+
